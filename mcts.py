e75e08132f0baa68a2a857cde79cd9f0a0d10068
Henry Routson <hroutson@student.unimelb.edu.au>
2024-04-29 22:50:05 +1000

    tiny

--- mcts.py
+++ mcts.py
@@ -1,4 +1,9 @@
 
+# https://pythontutor.com/visualize.html
+
+
+
+
 from typing import Optional
 import random
 
@@ -11,10 +16,11 @@ Action = int
 
 class GameTree : # / node
 
-  def __init__(self, children : list, winProp : WinsAndGames, state : State) -> None:
+  def __init__(self, children : list, winProp : WinsAndGames, action : Action) -> None:
     self.children : list[GameTree] = children
     self.winProp : WinsAndGames = winProp # win proportion
-    self.state = state
+    self.action = action 
+    # state is derived, as it takes too much space
 
 Children = list[GameTree]
 Path = list[GameTree]
@@ -34,7 +40,7 @@ def updatePathWinsAndGames(path : Path, didWin : bool) -> Path :
   return path
 
 
-def getMinOrMaxFromChildren(children : Children, isMax) -> GameTree :
+def getMinOrMaxFromChildren(children : Children, isMax) -> int :
 
   assert(children != [])
 
@@ -47,20 +53,21 @@ def getMinOrMaxFromChildren(children : Children, isMax) -> GameTree :
 
   max_index = scores.index(getValue)
   
-  return children[max_index]
+  return max_index
   
 
-def getMinMaxPath(tree : GameTree, isMaxFirst : bool) -> Path :
+def getMinMaxPath(tree : GameTree, isMaxFirst : bool) -> list[int] :
 
-  path : Path = []
+  path_i : list[int] = [] # path indexes
   while tree.children != [] :
 
-    next = getMinOrMaxFromChildren(tree.children, isMaxFirst)
-    path.append(next)
+    next_i = getMinOrMaxFromChildren(tree.children, isMaxFirst)
+    next = tree.children[next_i]
+    path_i.append(next_i)
     tree = next
     isMaxFirst = not isMaxFirst
 
-  return path
+  return path_i
 
 
 def playout(tree : GameTree) -> bool :
@@ -71,13 +78,6 @@ def playout(tree : GameTree) -> bool :
 
 # TODO simulate
 
-
-
-
-
-
-
-
 def getActionsFromState(state : State) -> list[Action] :
   return [1, -1]
 
@@ -85,29 +85,92 @@ def getActionsFromState(state : State) -> list[Action] :
 def applyActionToState(state : State, action : Action) -> State :
   return state + action
 
-def makeMove(state : State) -> Action :
-
-  actions = getActionsFromState(state)
-  for action in actions :
-    newState = applyActionToState(state, action)
-
-  # TODO
 
 
-  """
-
-  1 Selection 
-  2 Expansion 
-  3 Simulation
-  4 Back-propagation
-  """
 
 
-  return 1
+def rolloutStrategy(state : State, isMaxFirst: bool) :
+  # TODO test doing this random and improving this
+
+  action = random.choice(getActionsFromState(state))
+  #action = int(isMaxFirst)
+  
+  return action
+
+
+MAX_DEPTH = 9
+
+def rolloutSim(state : State, isMaxFirst: bool) -> bool :
+
+  while depth != MAX_DEPTH :
+
+    action = rolloutStrategy(state, isMaxFirst)
+    state = applyActionToState(state, action)
+
+    optionalWin = isStateWin(state, isMaxFirst)
+    if optionalWin != None : 
+      return optionalWin
+    
+    isMaxFirst = not isMaxFirst
+  
+    depth += 1
+
+ 
+  return tieBreaker(state)
+
 
   
-def isStateWin(state : State, isPlayer1 : bool) -> bool :
-  if isPlayer1 :
-    return state > 5
+
+def makeMoveWith(initState : State, tree : GameTree, isMaxFirst: bool) -> GameTree :
+
+  # 1 Selection (min max)
+  path_i = getMinMaxPath(tree, isMaxFirst)
+  depth = len(path_i)
+
+  # 2 Expansion (add a single node)
+
+  leafState = initState
+  leafNode = tree
+
+  for index in path_i :
+
+    leafState = applyActionToState(leafState, leafNode.action)
+    leafNode = leafNode.children[index]
+  
+
+  # TODO make more intelligent moves
+  action = rolloutStrategy(leafState, isMaxFirst)
+  leafNode.children.append(
+    GameTree([], (0, 0), action)
+  )
+
+  # TODO apply action to state
+
+  # 3 Simulation (rollout)
+ 
+  # 3.1 derive state
+  state = applyActionToState(leafState, action)
+
+  # 3.2 simluate rollout
+  result = rolloutSim(state, isMaxFirst)
+
+  # 4 Back-propagation (update win and games values)
+  path_i.append(0)
+  for index in path_i :
+    # update values
+    pass 
+    # TODO
+  
+  return tree
+
+  
+def isStateWin(state : State, isMax : bool) -> Optional[bool] :
+  if isMax :
+    if state > 5 : return True
   else :
-    return state < -5
+    if state < -5 : return False
+
+  return None
+  
+def tieBreaker(state : State) -> bool :
+  return state > 0 
