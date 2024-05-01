7187f480c897b65431d46f60aeeac98e0a9d317d
Henry Routson <hroutson@student.unimelb.edu.au>
2024-05-02 00:20:16 +1000

    tiny

--- mcts.py
+++ mcts.py
@@ -1,18 +1,16 @@
 
-PLAYER1 = "positive"
-PLAYER2 = "negative"
 MAX_DEPTH = 9
 START_STATE = 0
-
-
-
-
+EXPLORE_MIN = 13
+DEBUG = False
+C = 0.1 # from Upper Confidence Bound formula
+ITERATIONS = 1000
 
 from typing import Optional
 import random
 
 WinsAndGames = tuple[int, int]
-Player = str
+
 
 # define simple game for testing
 """
@@ -23,11 +21,65 @@ Player positive wins if the number if more positive
 Negative if negative
 
 """
+
+
+
+
+# ================================================================================
+# config 
+
+
+Player = str
+PLAYER1 = "positive"
+PLAYER2 = "negative"
+
+
 State = int
 Action = int
 
+def heuristic(state : State, action : Action, player : Player) -> float :
+  # used to pick which value to expand
+  # this is much better than expanding randomly
+
+  if player == PLAYER1 :
+    return action #* random.random()
+  else :
+    return -action
 
 
+def isStateWin(state : State) -> Optional[Player] :
+  if state > 5 : return PLAYER1
+  if state < -5 : return PLAYER2
+  return None
+  
+def tieBreaker(state : State) -> Player :
+
+  if state > 0 :
+    return PLAYER1
+  else :
+    return PLAYER2
+
+
+def getActionsFromState(state : State) -> list[Action] :
+  return [2, 1, -1, -2] # TODO test this and check win rate
+
+def applyActionToState(state : State, action : Action) -> State :
+  return state + action
+
+def rolloutStrategy(state : State, player: Player) :
+  action = random.choice(getActionsFromState(state))
+  return action
+
+
+
+
+
+
+
+
+
+# ================================================================================
+# impliment functions
 
 
 
@@ -50,8 +102,6 @@ def printTree(tree : GameTree, state : Optional[State], toIndent = 100, indent =
       tmpState = applyActionToState(state, t.action)
     printTree(t, tmpState, indent=(indent + 1), toIndent=toIndent)
 
-
-
 Children = list[GameTree]
 Path = list[GameTree]
 
@@ -71,12 +121,10 @@ def getMinOrMaxFromChildren(parent : GameTree, isMax) -> int :
   assert(children != [])
 
   # uncertainty
-
-
   e = list(enumerate(children))
   random.shuffle(e)
   for i, c in e :
-    if c.winProp[1] < 13 :  # explore unexplored
+    if c.winProp[1] < EXPLORE_MIN :  # explore unexplored
       return i
 
 
@@ -84,15 +132,12 @@ def getMinOrMaxFromChildren(parent : GameTree, isMax) -> int :
   scores = list(map(lambda child : UCB(Parent_n=parent, n=child), children))
   #scores = list(map(lambda child : scoreFromwinProp(child.winProp), children))
 
-
   if isMax : getValue = max(scores)
   else : getValue = min(scores)
 
   max_index = scores.index(getValue)
   return max_index
 
-
-# TODO test
 def getMinMaxPath(tree : GameTree, isMaxFirst : bool, state : State) -> tuple[list[GameTree], State] :
   
 
@@ -106,44 +151,21 @@ def getMinMaxPath(tree : GameTree, isMaxFirst : bool, state : State) -> tuple[li
     next = tree.children[next_i]
     path.append(next)
     tree = next
-    isMaxFirst = not isMaxFirst # may have mixed up negative
+    isMaxFirst = not isMaxFirst 
 
-
-  print("Path")
-  for c in path :
-    print(c.action)
-  print("Path end")
+  if DEBUG :
+    print("Path")
+    for c in path :
+      print(c.action)
+    print("Path end")
     
-
   return path, state
-
-
-
-testTree = GameTree(winProp=(1,1), children=[
-  GameTree(winProp=(2,1), children=[], action=None),
-  GameTree(winProp=(3,1), children=[   GameTree(winProp=(1,1), children=[], action=None),   GameTree(winProp=(0,1), children=[], action=None)], action=None)
-], action=None)
-
 
 
 def scoreFromTree(x : GameTree) :
   return scoreFromwinProp(x.winProp)
 
-def getActionsFromState(state : State) -> list[Action] :
-  return [2, 1, -1, -2] # TODO test this and check win rate
-
-def applyActionToState(state : State, action : Action) -> State :
-  return state + action
-
-def rolloutStrategy(state : State, player: Player) :
-  action = random.choice(getActionsFromState(state))
-  return action
-
-
-
 def rolloutSim(state : State, whosMove : Player, depth : int) -> Player :
-
-  # TODO while testing
 
   while depth != MAX_DEPTH :
 
@@ -166,15 +188,14 @@ def reversePlayer(player : Player) -> Player :
   if player == PLAYER2 : return PLAYER1
   assert(False)
 
-def heuristic(state : State, action : Action, player : Player) -> float :
-  # used to pick which value to expand
-  # this is much better than expanding randomly
+def whosMoveFromDepth(depth : int, playing : Player) -> Player :
 
-  if player == PLAYER1 :
-    return action #* random.random()
-  else :
-    return -action
+  # tree root has no action and has moves for the player deciding where to move
 
+  if (depth % 2 == 1) : 
+    return playing
+  else : 
+    return reversePlayer(playing)
 
 def makeMoveWith(initState : State, tree : GameTree, player: Player) -> GameTree :
   isMaxFirst = True
@@ -182,10 +203,7 @@ def makeMoveWith(initState : State, tree : GameTree, player: Player) -> GameTree
   # 1 Selection (min max)
   path, leafState = getMinMaxPath(tree, isMaxFirst, initState)
   depth = len(path)
-  if (depth % 2 == 1) : 
-    whosMove = PLAYER1
-  else : 
-    whosMove = PLAYER2
+  whosMove = whosMoveFromDepth(depth=depth, playing=player)
 
   # 2 Expansion (add a single node)
   leafNode = path[-1]
@@ -199,17 +217,16 @@ def makeMoveWith(initState : State, tree : GameTree, player: Player) -> GameTree
 
   leafNode.children.sort(key=heuristicFromChild, reverse=True)
 
-  print("children")
-  for t in leafNode.children: print(t.action)
-  print()
-
+  if DEBUG :
+    print("Children actions ranked")
+    for t in leafNode.children: print(t.action)
+    print()
 
   path.append(leafNode.children[0])
 
   # 3 Simulation (rollout)
 
 
- 
   # 3.1 derive state
   action = path[-1].action
   assert(action)
@@ -232,32 +249,16 @@ def N(t : GameTree) -> float : return float(t.winProp[1])
 
 def UCB(Parent_n : GameTree, n : GameTree) :
   assert(n in Parent_n.children)
-
-  C = 0.1
   return (U(n) / N(n)) + C * math.sqrt(math.log(N(Parent_n), 2) / N(n))
-  
-
-def isStateWin(state : State) -> Optional[Player] :
-  if state > 5 : return PLAYER1
-  if state < -5 : return PLAYER2
-  return None
-  
-def tieBreaker(state : State) -> Player :
-
-  if state > 0 :
-    return PLAYER1
-  else :
-    return PLAYER2
-
-
 
 def mcts(fromState : State = 0, iterations = 5000, player : Player = PLAYER1) -> Action :
 
   gameTree = GameTree([], (0,0), None) # starting node
   for _ in range(iterations) :
     gameTree = makeMoveWith(fromState, gameTree, player)
-    print("Tree")
-    printTree(gameTree, fromState, toIndent=3)
+    if DEBUG :
+      print("Tree")
+      printTree(gameTree, fromState, toIndent=3)
 
   nodes, endState = getMinMaxPath(gameTree, True, fromState)
   bestAction = nodes[1].action # 1 to ignore start node
@@ -265,12 +266,11 @@ def mcts(fromState : State = 0, iterations = 5000, player : Player = PLAYER1) ->
   return bestAction
 
 
-# call code =====
+
+# ================================================================================
+# call code
+
+print(mcts(iterations=ITERATIONS))
 
 
 
-print(mcts())
-
-
-
-# TODO add in formula for scoreFromwinProp
